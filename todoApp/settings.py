f8846db87e2db4e40e016505a282a5cf57e5496a
Kundan547 <kundanvyas197@gmail.com>
2025-05-24 10:07:46 +0530

    Change the ip.

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["44.201.218.90","localhost:80000"]
+ALLOWED_HOSTS = ["13.218.40.179","localhost:80000"]
 
 
 # Application definition
